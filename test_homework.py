27e8242efdc9d041072061eefbe995c36f98a792
alexey.kulikov <kulikov_alx@yandex.ru>
2024-07-21 21:52:08 +0400

    Добавил фомулы в "test_circle"

--- test_homework.py
+++ test_homework.py
@@ -39,12 +39,12 @@ def test_circle():
     """
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = pi * (r**2)
 
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2 * pi * r
 
     assert length == 144.51326206513048
 
